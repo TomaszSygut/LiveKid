d717a94eb76a6b081a4515cd48efd8c42ccd5589
TomaszSygut <thomassygut@gmail.com>
2021-01-14 00:31:02 +0100

    Fixed Filter

--- src/views/List/listHelper.js
+++ src/views/List/listHelper.js
@@ -1,9 +1,11 @@
 export const filterList = (item, search) => {
    const keysToFilter = ['name', 'status', 'tags']
    return Object.entries(item).some(
-      ([key, value]) =>
+      ([key, value]) => keysToFilter.includes(key) && value.includes(search.charAt(0).toUpperCase()) || keysToFilter[1].includes(key) && value.includes(search.charAt(0).toLowerCase()),
 
-         keysToFilter.includes(key) && value.includes(search)
+      //  keysToFilter.includes(key) && value.toLowerCase().includes(search) usunąłem wcześniejszy błąd z toLowerCase.
+      // w key "name" i "status" ustawiłem żeby zawsze pierwsza wpisywana litera była zamieniana w dużą a w key "status" żeby zawsze była mała.
+
 
    )
 
